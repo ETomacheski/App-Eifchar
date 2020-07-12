bad5d1d6cca8189f42be0145d993488edeefd28b
ETomacheski <tomacheski2001@gmail.com>
2020-07-12 20:30:14 -0300

    changing splash

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import 'intl';
+import React from 'react'
+import 'intl'
 import 'intl/locale-data/jsonp/pt-BR'
 
 import Routes from './src/routes'
 
-export default function App() {
+export default function App () {
   return (
     <Routes />
-  );
+  )
 }
-
